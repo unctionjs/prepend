4acedfdcc1a26fd60dfeafe6982dab7e16f8b709
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:06:18 -0700

    Don't bother using the extension, eslint complains

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import prepend from "./source.js"
+import plucks from "./source"
 
 test(({same, end}) => {
   same(prepend("a")(["b"]), ["a", "b"])
