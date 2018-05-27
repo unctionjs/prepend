6d58d381b4e8edde10240aa0f9a60b044d771517
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:30 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 /* eslint-disable no-extra-parens, no-unused-expressions */
-
-import type from "@unction/type"
-
-export default function prepend (value: any): Function {
-  return function prependLeft (orderedList: any): any {
+import type from "@unction/type";
+export default function prepend(value) {
+  return function prependLeft(orderedList) {
     switch (type(orderedList)) {
-      case "String": {
-        return `${value}${orderedList}`
-      }
-      case "Array": {
-        return [value, ...orderedList]
-      }
-      default: {
-        throw new Error(`prepend doesn't know how to deal with ${type(orderedList)}`)
-      }
+      case "String":
+        {
+          return `${value}${orderedList}`;
+        }
+
+      case "Array":
+        {
+          return [value, ...orderedList];
+        }
+
+      default:
+        {
+          throw new Error(`prepend doesn't know how to deal with ${type(orderedList)}`);
+        }
     }
-  }
+  };
 }
